07e138711dfd700a2e43c91985c9b153c474fa54
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-11-22 18:11:23 +0800

    re structuring directory

--- serverAndClientSideComp/next.config.ts
+++ serverAndClientSideComp/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    reactCompiler: true,
+    reactCompiler: true, // after installing the package for memoization
   experimental:{
       turbopackFileSystemCacheForDev: true,
   }
