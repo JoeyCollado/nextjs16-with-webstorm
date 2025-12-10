a8d42f82e717f70f705a35133843104251c510b6
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-12-10 15:12:07 +0800

    event card details rendered

--- devent/app/components/EventCard.tsx
+++ devent/app/components/EventCard.tsx
@@ -1,24 +1,46 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 interface Props {
-    title: string;
-    image: string;
-    slug: string;
-    location: string;
-    date: string;
-    time: string;
+  title: string;
+  image: string;
+  slug: string;
+  location: string;
+  date: string;
+  time: string;
 }
 
-const EventCard = ({title, image} : Props) => {
+const EventCard = ({ title, image, slug, location, date, time }: Props) => {
   return (
-    <Link href={`/events`} id='event-card'>
-        <Image src={image} alt={title} width={410} height={300} className='poster'/>
+    <Link href={`/events/${slug}`} id="event-card">
+      <Image
+        src={image}
+        alt={title}
+        width={410}
+        height={300}
+        className="poster"
+      />
 
-        <p className='title'>{title}</p>
+      <div className="flex flex-row gap-2">
+        <Image src="/icons/pin.svg" alt="location" width={14} height={14} />
+        <p>{location}</p>
+      </div>
+
+      <div className="datetime">
+        <div className="">
+          <Image src="/icons/calendar.svg" alt="date" width={14} height={14} />
+          <p>{date}</p>
+        </div>
+
+        <div className="">
+          <Image src="/icons/clock.svg" alt="time" width={14} height={14} />
+          <p>{time}</p>
+        </div>
+      </div>
+      <p className="title">{title}</p>
     </Link>
-  )
-}
+  );
+};
 
-export default EventCard
+export default EventCard;
